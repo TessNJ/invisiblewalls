f0281247f93b84382a4536d4d8db9325ed25ae41
Tess <tere0506@stud.kea.dk>
2023-04-20 14:41:58 +0200

    header v1

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,7 +6,8 @@ function Layout({ children }) {
   let logoWidth = "100";
   let logoHeight = "60";
 
-  function unfoldMenu() {
+  function unfoldMenu(event) {
+    event.target.classList.toggle("active");
     document.querySelector(".navMenu").classList.toggle("hideMenu");
   }
 
@@ -16,7 +17,7 @@ function Layout({ children }) {
         <Image alt="" src={"https://picsum.photos/" + logoWidth + "/" + logoHeight} width={logoWidth} height={logoHeight} />
         <div className="burgerMenu" onClick={unfoldMenu}>
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+            <path fillRule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1.5H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1.5H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1.5H3a.5.5 0 0 1-.5-.5z" />
           </svg>
         </div>
         <nav className="navMenu hideMenu">
