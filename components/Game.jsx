4cdf0b668981eb26eca927bbff4471383de01a7a
Tess <teresenj@live.dk>
2023-04-25 14:41:22 +0200

    games page edits

--- components/Game.jsx
+++ components/Game.jsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 export default function Game({ children }) {
   return (
     <article>
-      <Image alt="" src={"https://picsum.photos/" + 500} width="500" height="500"></Image>
+      <Image alt="" src={"https://picsum.photos/" + 1920 + "/" + 600} width="1000" height="600"></Image>
       <div>
         <h4> Game {children}</h4>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit obcaecati modi nesciunt optio soluta voluptatum fuga quam repellendus temporibus impedit, sunt minima iste veritatis dolor ipsam voluptatibus id dicta suscipit!</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit obcaecati modi nesciunt optio soluta voluptatum fuga quam repellendus temporibus impedit, sunt minima iste veritatis dolor ipsam voluptatibus id dicta suscipit!</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit obcaecati modi nesciunt optio soluta voluptatum fuga quam repellendus temporibus impedit, sunt minima iste veritatis dolor ipsam voluptatibus id dicta suscipit!</p>
-        <button>Take me to site</button>
+        <button className="buttonSplash ">Take me to site</button>
       </div>
     </article>
   );
