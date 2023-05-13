21afe519d0e420a30c350fbd53c8e834c8100f5d
Tess <tere0506@stud.kea.dk>
2023-05-13 21:28:34 +0200

    slight styling + cleanup

--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -11,9 +11,9 @@ export default function Privacypolicy() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <main>
+      <main className="privacyPolicy">
         <section>
-          <h1>Currently Under Review</h1>
+          <h1>\\ Currently Under Review</h1>
           <p>Our privacy policy is currently under review before we update it. It will return shortly.</p>
           <p>We appologize for any inconvinience this may cause.</p>
         </section>
