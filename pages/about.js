2440a0d6430b3ba6cbd6395ec50699c85929957e
Tess <tere0506@stud.kea.dk>
2023-05-29 15:23:07 +0200

    small cleanup

--- pages/about.js
+++ pages/about.js
@@ -95,7 +95,7 @@ export default function About({ data }) {
   );
 }
 
-export async function getStaticProps(contect) {
+export async function getStaticProps() {
   const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxramt6a3pxdGZod2Z5bmh3enh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIzNDkxMDAsImV4cCI6MTk5NzkyNTEwMH0.dJ3RCc9bmU3C0gFQq22hbDfXdzxUXETNiU8Yrknx4y8";
   const url = "https://lkjkzkzqtfhwfynhwzxx.supabase.co/";
   const res = await fetch(url + "rest/v1/iwemployees", {
--- pages/games.js
+++ pages/games.js
@@ -28,7 +28,7 @@ export default function Games({ data }) {
   );
 }
 
-export async function getStaticProps(contect) {
+export async function getStaticProps() {
   const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxramt6a3pxdGZod2Z5bmh3enh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIzNDkxMDAsImV4cCI6MTk5NzkyNTEwMH0.dJ3RCc9bmU3C0gFQq22hbDfXdzxUXETNiU8Yrknx4y8";
   const url = "https://lkjkzkzqtfhwfynhwzxx.supabase.co/";
   const res = await fetch(url + "rest/v1/iwgames", {
